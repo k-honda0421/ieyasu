ac0dec398b158bf966e3d629f41e2a3231a3e0ac
けん <keiju.love@gmail.com>
2020-06-17 18:12:18 +0900

    投稿機能作成

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,14 @@
 
 from django.urls import path
 
-from myapp.views import IndexView, PostDetailView, CategoryListView, TagListView
-
+from myapp.views import (
+    IndexView,
+    PostDetailView,
+    CategoryListView,
+    TagListView,
+    TagPostView,
+    CategoryPostView
+)
 app_name = 'myapp'
 urlpatterns = [
     # 記事一覧のURL
@@ -12,6 +18,11 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     # カテゴリー一覧のURL
     path('categories/', CategoryListView.as_view(), name='category_list'),
-    # タグ一覧のビュー
+    # タグ一覧のURL
     path('tags/', TagListView.as_view(), name='tag_list'),
+    # カテゴリに紐づいた記事の一覧のURL
+    path('category/<str:category_slug>/',
+        CategoryPostView.as_view(), name='category_post'),
+    # タグに紐づいた記事の一覧のURL
+    path('tag/<str:tag_slug>/',TagPostView.as_view(),name='tag_post'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count,Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -34,4 +34,38 @@ class CategoryListView(ListView):
 class TagListView(ListView):
     # 公開されている記事のタグ数を取得している
     queryset = Tag.objects.annotate(
-        num_posts=Count('post', filter=Q(post__is_public=True)))    
+        num_posts=Count('post', filter=Q(post__is_public=True)))
+
+# カテゴリに紐づいた記事の一覧のビュー
+class CategoryPostView(ListView):
+    model = Post
+    template_name = 'myapp/category_post.html'
+
+    # カテゴリに紐づいた記事を取得するメソッド
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        self.category = get_object_or_404(Category, slug=category_slug)
+        qs = super().get_queryset().filter(category=self.category)
+        return qs
+
+    # 上記メソッドで取得したデータをテンプレートへ渡すメソッド
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+# タグに紐づいた記事の一覧のビュー
+class TagPostView(ListView):
+    model = Post
+    template_name = 'myapp/tag_post.html'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        qs = super().get_queryset().filter(tags=self.tag)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
